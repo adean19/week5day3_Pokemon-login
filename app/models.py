4a9948675785cf560c340af4c22dbbbbd9a72c07
Nickero Dean <nickerodean@Nickeros-MacBook-Air.local>
2023-07-16 09:26:14 -0400

    Building out the catch functionality

--- app/models.py
+++ app/models.py
@@ -5,20 +5,45 @@ from flask_login import UserMixin
 db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key= True)
+    user_id = db.Column(db.Integer, primary_key= True)
     username = db.Column(db.String(45), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
-    # pokemon
 
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.password = password
 
-# class Pokemon(db.Model):
-#     id = db.Column(db.Integer, primary_key= True)
-#     name = db.Column(db.String(45), nullable=False, unique=True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def get_id(self):
+        return (self.user_id)
+
+class Pokemon(db.Model):
+    pokemon_id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String(45), nullable=False, unique=True)
+    ability = db.Column(db.String, nullable=False)
+    hp = db.Column(db.String, nullable=False)
+    attack = db.Column(db.String, nullable=False)
+    defense = db.Column(db.String, nullable=False)
+    sprite = db.Column(db.String, nullable=False)
+    date_discovered = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+
+    def __init__(self, name, ability, hp, attack, defense, sprite):
+        self.name = name
+        self.ability = ability
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+        self.sprite = sprite
+
+class Caught_Pokemon(db.Model):
+    catch_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    pokemon_id = db.Column(db.Integer, db.ForeignKey('pokemon.pokemon_id'))
+    date_caught = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+
+    def __init__(self, user_id, pokemon_id):
+        self.user_id = user_id
+        self.pokemon_id = pokemon_id
 
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, request, redirect, url_for, flash
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from .forms import PokemonForm, SignUpForm, LoginForm
-from .models import User, db
+from .models import User, Pokemon, Caught_Pokemon, db
 import requests, json
 
 @app.route('/', methods=["GET", "POST"])
@@ -74,14 +74,46 @@ def pokemon_search():
         pokemon_apicall = f'https://pokeapi.co/api/v2/pokemon/{form.pokemon_name.data.lower()}'
         pokemon_apicall_response = requests.get(pokemon_apicall)
         if pokemon_apicall_response.ok == True:
+            # Call pokemon data from API
             pokemon_data = {'Name': pokemon_apicall_response.json()['forms'][0]['name'],
                             'Ability': pokemon_apicall_response.json()['abilities'][0]['ability']['name'],
                             'Stats': {'HP': pokemon_apicall_response.json()['stats'][0]['base_stat'],
                                       'Defense': pokemon_apicall_response.json()['stats'][2]['base_stat'],
                                       'Attack' : pokemon_apicall_response.json()['stats'][1]['base_stat']},
                             'Sprite': pokemon_apicall_response.json()['sprites']['front_shiny']}
+            # Setting pokemon_data to individual variables
+            pokemon_name = pokemon_data['Name']
+            pokemon_ability = pokemon_data['Ability']
+            pokemon_hp = pokemon_data['Stats']['HP']
+            pokemon_attack = pokemon_data['Stats']['Attack']
+            pokemon_defense = pokemon_data['Stats']['Defense']
+            pokemon_sprite = pokemon_data['Sprite']
+            # Setting individual pokemon variables to class
+            pokemon_info = Pokemon(pokemon_name, pokemon_ability, pokemon_hp, pokemon_attack, pokemon_defense, pokemon_sprite)
+            # Add pokemon info to db
+            if not Pokemon.query.filter_by(name=pokemon_name).first():
+                db.session.add(pokemon_info)
+                db.session.commit()
     return render_template('pokemon_search.html', title='Pokemon', form=form, pokemon_data=pokemon_data)
 
-#My Team
+#Catching Pokemon
+
+@app.route('/pokemonsearch/catchpokemon/<pokemon_id>', methods=["GET", "POST"])
+@login_required
+def catch_pokemon(pokemon_id):
+    # Query the db and assign the result to the catch variable
+    catch = Caught_Pokemon.query.filter_by(pokemon_id=Pokemon.pokemon_id).filter_by(user_id=current_user.user_id).first()
+    print(catch)
+    # if catch = true/if value is found, redirect to the pokemon search page
+    if catch:
+        return redirect(url_for('pokemon_search'))
+    # store the caught_pokemon instance into a variable
+    catch = Caught_Pokemon(current_user.user_id, pokemon_id)
+    print(catch)
+    # add the data from the catch variable to the db
+    db.session.add(catch)
+    db.session.commit()
+    flash('X has been added to your team.')
+    return redirect(url_for('pokemon_search'))
 
 #Battling Pokemon
